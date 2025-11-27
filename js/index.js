e174bd4bc48898df4e59be684864c25dfb0f2cca
codewithsathwik <sathvikms344@gmail.com>
2025-11-28 02:55:17 +0530

    added new changes to mobile navigation

--- js/index.js
+++ js/index.js
@@ -1,29 +1,58 @@
 // navgation bar toggle for mobile
 let toggleBtn = document.querySelector(".menu-btn");
 let navMenu = document.querySelector(".nav");
+let aTag = document.querySelectorAll(".nav a");
 
 
 if (toggleBtn) {
-    toggleBtn.addEventListener("click", () => {
-        let arExp = toggleBtn.getAttribute("aria-expanded");
+    // menu button click
+    let arExp;
+    toggleBtn.addEventListener("click", (e) => {
+        e.stopPropagation(); // Prevent window click
+        arExp = toggleBtn.getAttribute("aria-expanded");
         console.log(arExp);
         if (arExp === "false") {
             toggleBtn.setAttribute('aria-expanded', String("true"));
-            navMenu.style.display = "inline-flex";
+            navMenu.classList.add("display");
             navMenu.classList.remove("ani-close");
             navMenu.classList.add("ani-open");
-            toggleBtn.textContent = "✖";
         } else {
-            toggleBtn.setAttribute('aria-expanded', String("false"));
-            navMenu.classList.remove("ani-open");
-            navMenu.classList.add("ani-close");
-            toggleBtn.textContent = "☰";
-            navMenu.addEventListener("animationend", (e) => {
-                if (e.animationName === "slide-right-normal") {
-                    navMenu.style.display = "none";
-                    
-                }
-            });
+            navCloss();
         }
     });
+
+    // the below is used to clode mobile nav after animation
+    navMenu.addEventListener("animationend", (e) => {
+        if (e.animationName === "slide-right-normal") {
+            navMenu.classList.remove("display");
+        }
+    });
+
+    //function to close navbar smoothly wiht animation
+    function navCloss() {
+        toggleBtn.setAttribute('aria-expanded', String("false"));
+        navMenu.classList.remove("ani-open");
+        navMenu.classList.add("ani-close");
+    }
+
+    // hide mode navbar when resized
+    window.addEventListener("resize", () => {
+        if (arExp == "true" && window.innerWidth < 1050) {
+            navCloss();
+        }
+    });
+
+    //used to close nav bar when clicked on outside of navbar
+    window.addEventListener("click", (e) => {
+        if (e.target.tagName != "NAV" && navMenu.classList.contains('display')) {
+            navCloss();
+        }
+    });
+
+    aTag.forEach((a) => {
+        a.addEventListener("click", () => {
+            navCloss();
+        })
+    })
+
 }
