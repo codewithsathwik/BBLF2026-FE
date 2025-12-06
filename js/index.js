e3bffe4dc0cbed5bf5923e4d88372b0ba41e31c3
codewithsathwik <sathvikms344@gmail.com>
2025-12-07 04:52:58 +0530

    added video gallery

--- js/index.js
+++ js/index.js
@@ -211,7 +211,7 @@ function swapingSections(button) {
         allSection.forEach(section => {
             let sectionId = section.id;
             if (sectionId === targetId) {
-                allSection.forEach((sec) =>{ 
+                allSection.forEach((sec) => {
                     sec.classList.remove("speakers-show");
                     sec.classList.remove("slide-speaker");
                 });
@@ -265,7 +265,72 @@ function openPDF(path) {
         setTimeout(removeLoader, 1500);
     };
 
-    function removeLoader(){
+    function removeLoader() {
         pdfContainer.classList.remove("loading");
     }
 }
+
+
+// ================sliders================
+
+const playCards = document.querySelectorAll(".play-card");
+const nextBtn = document.querySelector(".next");
+const prevBtn = document.querySelector(".prev");
+const playlistFrame = document.querySelector(".video-iframe iframe");
+const allCards = document.querySelector(".sliders")
+
+
+const firstCard = playCards[0];
+const totalWidth = firstCard.parentElement.scrollWidth;
+const visibleWidth = firstCard.parentElement.clientWidth;
+const maxOffset = totalWidth - visibleWidth;
+
+let offset = 0;
+
+const defaultSize = 150; // in px 
+
+playCards.forEach(card => {
+    card.addEventListener("click", () => {
+        playCards.forEach(crd => crd.classList.remove("active-slide"));
+        card.classList.add("active-slide");
+        let link = card.getAttribute("data-target");
+        changePlaylistSrc(link);
+    })
+});
+
+function changePlaylistSrc(link) {
+    playlistFrame.setAttribute("src", link);
+}
+
+function applyTransform() {
+    playCards.forEach(card => {
+        card.style.transform = `translateX(-${offset}px)`;
+    });
+}
+
+nextBtn.addEventListener("click", () => {
+    offset = Math.min(offset + defaultSize, maxOffset);
+    applyTransform();
+});
+
+prevBtn.addEventListener("click", () => {
+    offset = Math.max(offset - defaultSize, 0);
+    applyTransform();
+});
+
+
+// function getYouTubeTitle(videoUrl) {
+//     const videoId = videoUrl.split('/embed/')[1].split('?')[0];
+//     const oembedUrl = `https://www.youtube.com/oembed?url=https://www.youtube.com/watch?v=${videoId}&format=json`;
+
+//     fetch(oembedUrl)
+//         .then(response => response.json())
+//         .then(data => {
+//             return data.title;
+//         })
+//         .catch(error => console.error('Error:', error));
+// }
+
+// console.log(getYouTubeTitle("https://www.youtube.com/embed/GenHXHOSUqc"));
+
+
